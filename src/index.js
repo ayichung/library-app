afe7a08673174f1b560db511dd96120e27563dda
ayichung <121764547+ayichung@users.noreply.github.com>
2023-08-05 20:21:07 -0400

    Write style for book elements

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import Ui from './ui';
 
 // startup
 const ui = new Ui();
-document.addEventListener('DOMContentLoaded', ui.loadLibrary());
+document.addEventListener('DOMContentLoaded', ui.loadList());
--- src/ui.js
+++ src/ui.js
@@ -2,21 +2,52 @@ import Book from './book.js';
 import Library from './library.js';
 
 export default class Ui {
-    loadLibrary () {
+    loadList () {
         // testing setup
         const library = new Library();
         const book = new Book("Circe", "Madeline Miller", 416);
         library.addBookToLibrary(book);
+        this.clearList();  // this should go in add/delete methods
 
-        // here
-        const books = library.getLibrary();
-        console.log(books);
-        // do dom stuff
+        // here, just finished implement load list
+        // next, implement add book for storage
+        // and then add book from input fields
+        // and then delete stuff
+        const list = document.querySelector(".book-list");
+        const lib = library.getLibrary();
+        for (const i in lib) {
+            console.log(lib[i]);
+            const bookElem = this.createBookElem(book);
+            list.appendChild(bookElem);
+        }
     }
 
-    // clearLibrary () {
+    createBookElem (book) {
+        const bookElem = document.createElement("div");
+        const title = document.createElement("p")
+        const author = document.createElement("p")
+        const pages = document.createElement("p")
+        const rmBtn = document.createElement("button")
 
-    // }
+        bookElem.classList.add("book");
+        title.classList.add("title");
+        title.textContent = book.title;
+        author.textContent = "by " + book.author;
+        pages.textContent = book.pages + " pages";
+        rmBtn.textContent = "remove";
+
+        bookElem.appendChild(title);
+        bookElem.appendChild(author);
+        bookElem.appendChild(pages);
+        bookElem.appendChild(rmBtn);
+
+        return bookElem;
+    }
+
+    clearList () {
+        const list = document.querySelector(".book-list");
+        list.textContent = null;
+    }
 
     // listen for submit btn
         // new book
@@ -37,4 +68,4 @@ export default class Ui {
     // }
 }
 
-export const { loadLibrary } = new Ui();
+export const { loadList: loadLibrary } = new Ui();
